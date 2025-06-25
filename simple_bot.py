30780ff548e5792cd9649a3aed0dd9269e5fb761
LMDIFU <lemidoitforyou@gmail.com>
2025-06-25 14:26:28 +0400

    Fix event loop error

--- simple_bot.py
+++ simple_bot.py
@@ -15,14 +15,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(update.message.text)
 
-async def main():
+def main() -> None:
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), echo))
 
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    main()
